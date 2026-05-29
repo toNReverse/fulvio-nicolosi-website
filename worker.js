b59da675bffd4cf0e41155202689984aad8b1147
toNReverse <mark04sap@gmail.com>
2026-05-29 15:23:10 +0200

    Mostra link biglietti anche per i concerti passati

--- worker.js
+++ worker.js
@@ -1,24 +1,34 @@
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Notion-Version',
+};
+
 export default {
-    async fetch(request, env) {
-      const url = new URL(request.url);
-      const notionUrl = 'https://api.notion.com' + url.pathname + url.search;
-  
-      const res = await fetch(notionUrl, {
-        method: request.method,
-        headers: {
-          'Authorization': `Bearer ${env.NOTION_TOKEN}`,
-          'Notion-Version': '2022-06-28',
-          'Content-Type': 'application/json',
-        },
-        body: request.method !== 'GET' ? request.body : undefined,
-      });
-  
-      const data = await res.json();
-      return new Response(JSON.stringify(data), {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        }
-      });
+  async fetch(request, env) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { status: 204, headers: CORS });
     }
+
+    const url = new URL(request.url);
+    const notionUrl = 'https://api.notion.com' + url.pathname + url.search;
+
+    const res = await fetch(notionUrl, {
+      method: request.method,
+      headers: {
+        'Authorization': `Bearer ${env.NOTION_TOKEN}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json',
+      },
+      body: request.method !== 'GET' ? request.body : undefined,
+    });
+
+    const data = await res.json();
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        ...CORS,
+      },
+    });
   }
+}
